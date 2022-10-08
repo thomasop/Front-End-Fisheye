445fc78259857923dc6052b7b2a1d8e321513ff1
thomasop <tdss33@hotmail.com>
2022-10-08 10:29:15 +0200

    Add: add all media in asset and using fetch for import data

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { id, name, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
